40af26668e7836f61bf64e8d5ab34d91add0c8cf
web180730 <44466616+web180730@users.noreply.github.com>
2018-11-06 15:14:48 +0800

    Add files via upload

---
+++ home/LoginContainer.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class='app-logincontainer'>
+         <form action="#">
+             <input type="text"  name="uname" v-model="unameval">
+             <input type="password" name="upwd" v-model="upwdval">
+             <input type="button" id='btn' value="登录"@click='btnSubmit()'>
+         </form>
+    </div>
+</template>
+<script>
+import{Toast} from 'mint-ui'
+    export default{
+        data(){
+            return {
+                unameval:'',
+                upwdval:''
+            }
+        },
+        methods:{
+            btnSubmit(){
+                var u=this.unameval;
+                var p=this.upwdval;
+                this.$http.get('login?uname='+u+'&upwd='+p).then(result=>{
+                    //console.log(result)
+                    if(result.body.msg==1){
+                        Toast(result.body.msg)
+                        this.$router.push('/');
+                    }else{
+                        Toast(result.body.msg);
+                    }
+                })
+            },
+        },
+    }
+</script>
+<style>
+</style>
